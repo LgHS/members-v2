f2a5784b1315e73381c2d7b352f1a9b843b85a7a
Duman Haydar <haydar@duman.io>
2022-06-05 21:00:31 +0200

    add description column readers table

---
+++ database/migrations/2022_06_05_185818_update_readers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(Schema::hasTable('readers')) {
+            Schema::table('readers', function (Blueprint $table) {
+                $table->text('description', '256');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if(Schema::hasTable('readers')) {
+            Schema::table('readers', function (Blueprint $table) {
+                $table->dropColumn('description');
+            });
+        }
+    }
+};
